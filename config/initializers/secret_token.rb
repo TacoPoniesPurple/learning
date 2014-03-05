726fef648b020d74a7091356602d7646324185fb
Taco <tacoponiespurple@gmail.com>
2014-03-05 16:22:41 -0700

    Commit inicial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BranchingApp::Application.config.secret_key_base = '6879d51b536e9614f635384710420018b65885a0d66f38924612bcbf7d4bbe6808368e2b812089818cc2492f7ff8094f3d6c780e4174801f5bdadab2e0c1f639'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BranchingApp::Application.config.session_store :cookie_store, key: '_branching_app_session'
